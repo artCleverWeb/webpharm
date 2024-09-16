0be66598793e3e9463bf922e3df85f2e999faa08
Андрей Лебедев <la@kolos.studio>
2024-09-16 18:35:43 +0300

    Читстовая установка битрикса

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'WEBPHARM | Центр дистанционного обучения',
+	'description' => '',
+	'keywords' => '',
+	'robots' => 'index, follow'
+);
+?>
---
+++ local/templates/wph_index/description.php
@@ -0,0 +1,9 @@
+<?
+if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
+	die();
+
+$arTemplate = Array(
+	'NAME' => 'WebPharm.ru. Авторизованный пользователь',
+	'DESCRIPTION' => ''
+);
+?>
