95598c10eba1f279494b6e73cc8cd9a935420fae
Андрей Лебедев <la@kolos.studio>
2024-09-23 19:50:51 +0300

    Добавление футера и перенос фронта

--- local/templates/wph_index/footer.php
+++ local/templates/wph_index/footer.php
@@ -1,6 +1,8 @@
 <?
+/** @global $APPLICATION */
 if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
 	die();
 ?>
+        </div>
 	</body>
 </html>
--- local/templates/wph_index/header.php
+++ local/templates/wph_index/header.php
@@ -24,6 +24,11 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
           rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap"
           rel="stylesheet">
+    <script defer src="/assets/js/runtime.bundle.js"></script>
+    <script defer src="/assets/js/vendors.bundle.js"></script>
+    <script defer src="/assets/js/main.bundle.js"></script>
+    <link href="/assets/css/main.css" rel="stylesheet"></head>
+
 </head>
 <body>
 <div id="panel">
@@ -50,7 +55,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
                     <div class="navbar__grid-item navbar__grid-item_3">
                         <div class="notices notices_navbar navbar__notices">
                             <div class="notices__icon">
-                                <svg class="icon notices__icon-canvas"><use xlink:href="/assets/images/icons/icons.svg#icon-bell"></use></svg>
+                                <svg class="icon notices__icon-canvas"><use xlink:href="images/icons/icons.svg#icon-bell"></use></svg>
                                 <div class="notices__indicator"></div>
                             </div>
                         </div>
@@ -64,7 +69,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
             </a>
             <a href="#" class="user-mini user-mini_navbar navbar__user">
                 <div class="user-mini__picture">
-                    <img src="/assets/upload//assets/images/1-1.png" alt="" class="user-mini__picture-img">
+                    <img src="/assets/upload/images/1-1.png" alt="" class="user-mini__picture-img">
                 </div>
                 <div class="user-mini__data">
                     <div class="user-mini__name">
@@ -77,60 +82,20 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
                     <div class="menu-main__item active">
                         <a href="#" class="menu-main__item-link">
                             <div class="menu-main__item-link-icon">
-                                <svg class="icon menu-main__item-link-icon-canvas"><use xlink:href="/assets/images/icons/icons.svg#icon-study"></use></svg>
+                                <svg class="icon menu-main__item-link-icon-canvas"><use xlink:href="images/icons/icons.svg#icon-study"></use></svg>
                             </div>
                             <div class="menu-main__item-link-text">
-                                Карьера
+                                Как начать работать
                             </div>
                         </a>
                     </div>
                     <div class="menu-main__item">
                         <a href="#" class="menu-main__item-link">
                             <div class="menu-main__item-link-icon">
-                                <svg class="icon menu-main__item-link-icon-canvas"><use xlink:href="/assets/images/icons/icons.svg#icon-smile"></use></svg>
-                            </div>
-                            <div class="menu-main__item-link-text">
-                                Мы заботимся о вас
-                            </div>
-                        </a>
-                    </div>
-                    <div class="menu-main__item">
-                        <a href="#" class="menu-main__item-link">
-                            <div class="menu-main__item-link-icon">
-                                <svg class="icon menu-main__item-link-icon-canvas"><use xlink:href="/assets/images/icons/icons.svg#icon-calendar"></use></svg>
-                            </div>
-                            <div class="menu-main__item-link-text">
-                                События
-                            </div>
-                        </a>
-                    </div>
-                    <div class="menu-main__item">
-                        <a href="#" class="menu-main__item-link">
-                            <div class="menu-main__item-link-icon">
-                                <svg class="icon menu-main__item-link-icon-canvas"><use xlink:href="/assets/images/icons/icons.svg#icon-journal"></use></svg>
-                            </div>
-                            <div class="menu-main__item-link-text">
-                                Журнал
-                            </div>
-                        </a>
-                    </div>
-                    <div class="menu-main__item">
-                        <a href="#" class="menu-main__item-link">
-                            <div class="menu-main__item-link-icon">
-                                <svg class="icon menu-main__item-link-icon-canvas"><use xlink:href="/assets/images/icons/icons.svg#icon-ruble"></use></svg>
+                                <svg class="icon menu-main__item-link-icon-canvas"><use xlink:href="images/icons/icons.svg#icon-ruble"></use></svg>
                             </div>
                             <div class="menu-main__item-link-text">
                                 Заработать больше
-                            </div>
-                        </a>
-                    </div>
-                    <div class="menu-main__item">
-                        <a href="#" class="menu-main__item-link">
-                            <div class="menu-main__item-link-icon">
-                                <svg class="icon menu-main__item-link-icon-canvas"><use xlink:href="/assets/images/icons/icons.svg#icon-ruble-x"></use></svg>
-                            </div>
-                            <div class="menu-main__item-link-text">
-                                Заработать еще больше
                             </div>
                         </a>
                     </div>
@@ -139,7 +104,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 
             <div class="navbar__footer">
                 <label class="input-button-a navbar__option">
-                    <input type="checkbox" class="input-button-a__input" checked disabled />
+                    <input type="checkbox" class="input-button-a__input" disabled />
                     <span class="input-button-a__icon"></span>
                     <span class="input-button-a__text">
 					Опытный сотрудник
@@ -148,3 +113,4 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
             </div>
         </div>
     </div>
+    <div class="cnt page__cnt">
