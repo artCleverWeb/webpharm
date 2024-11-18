083aa227b434baeb85c87da7bb5f6a12c1bd653b
Андрей Лебедев <la@kolos.studio>
2024-11-18 21:06:24 +0300

    Шаблон ЛК пользователя

--- local/components/kolos.studio/user.wallet/templates/.default/template.php
+++ local/components/kolos.studio/user.wallet/templates/.default/template.php
@@ -11,70 +11,71 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
 \Bitrix\Main\UI\Extension::load('ui.vue3');
 ?>
 
-<div class="grid grid_layout" id="appWallet">
-    <div class="grid__item grid__item_1">
-        <div class="content">
-            <div class="cabinet">
-                <div class="header header_margin-1 content__header">
-                    <a href="#" class="header__nav-button header__nav-button_back">
-                        <svg class="icon header__nav-button-icon"><use xlink:href="/assets/images/icons/icons.svg#icon-arrow-2"></use></svg>
-                    </a>
-                    <div class="headline">
-                        <h1 class="title title_size-2 headline__title">
-                            Мой кошелёк
-                        </h1>
-                    </div>
-                    <div class="header__text">
-                        <p>
-                            Баллы в «Мой кошелёк» – начисляются за выполнение приказа об обязательном обучении.<br/>
-                            Из «Моего кошелька» периодически (раз в месяц) происходит списание баллов и их перевод в «премию за квалификацию».
-                            Баллы рейтинговые – начисляются за активность на портале (за происхождение информационных и обязательных обучений).
-                        </p>
-                    </div>
+
+<div class="header header_margin-1 content__header">
+    <a href="/" class="header__nav-button header__nav-button_back">
+        <svg class="icon header__nav-button-icon">
+            <use xlink:href="/assets/images/icons/icons.svg#icon-arrow-2"></use>
+        </svg>
+    </a>
+    <div class="headline">
+        <h1 class="title title_size-2 headline__title">
+            Мой кошелёк
+        </h1>
+    </div>
+    <div class="header__text">
+        <p>
+            Баллы в «Мой кошелёк» – начисляются за выполнение приказа об обязательном обучении.<br/>
+            Из «Моего кошелька» периодически (раз в месяц) происходит списание баллов и их перевод в «премию за
+            квалификацию».
+            Баллы рейтинговые – начисляются за активность на портале (за происхождение информационных и обязательных
+            обучений).
+        </p>
+    </div>
+</div>
+<div class="wallet cabinet__wallet js-wallet" id="appWallet">
+    <div class="wallet__tabs">
+        <div class="menu-b wallet__tabs-nav">
+            <div class="menu-b__inner">
+                <div class="menu-b__list wallet__tabs-nav-list">
+                    <button class="menu-b__item wallet__tabs-nav-item active">
+                        Все
+                    </button>
+                    <button class="menu-b__item wallet__tabs-nav-item">
+                        Баллы
+                    </button>
+                    <button class="menu-b__item wallet__tabs-nav-item">
+                        Рубли
+                    </button>
                 </div>
-                <div class="wallet cabinet__wallet js-wallet">
-                    <div class="wallet__tabs">
-                        <div class="menu-b wallet__tabs-nav">
-                            <div class="menu-b__inner">
-                                <div class="menu-b__list wallet__tabs-nav-list">
-                                    <button class="menu-b__item wallet__tabs-nav-item active">
-                                        Все
-                                    </button>
-                                    <button class="menu-b__item wallet__tabs-nav-item">
-                                        Баллы
-                                    </button>
-                                    <button class="menu-b__item wallet__tabs-nav-item">
-                                        Рубли
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="wallet__tabs-content">
-                            <div class="wallet__tabs-content-item active">
-                                <div class="transactions wallet__transactions" data-autoload="true">
-                                    <div class="transactions__list" v-for="item in allList">
-                                        <div class="transaction-mini transactions__item" :class="{'transaction-mini_minus': item.direction == 'down', 'transaction-mini_plus': item.direction == 'up'}">
-                                            <div class="transaction-mini__grid transaction-mini__grid_1">
-                                                <div class="transaction-mini__grid-item transaction-mini__grid-item_1">
-                                                    <div class="transaction-mini__title" v-html="item.name" v-if="item.url.length < 1">
-                                                    </div>
-                                                    <a :href="item.url" class="transaction-mini__title" v-html="item.name" v-if="item.url.length > 1">
-                                                    </a>
-                                                    <div class="transaction-mini__number">
-                                                        <div class="transaction-mini__number-icon">
-                                                            <svg class="icon transaction-mini__number-icon-canvas"><use xlink:href="/assets/images/icons/icons.svg#icon-arrow-2"></use></svg>
-                                                        </div>
-                                                             <div class="transaction-mini__number-value">
-                                                            +50 баллов
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="transaction-mini__grid-item transaction-mini__grid-item_2">
-                                                    <div class="transaction-mini__date" v-html="item.date">
-                                                         </div>
-                                                </div>
-                                            </div>
+            </div>
+        </div>
+        <div class="wallet__tabs-content">
+            <div class="wallet__tabs-content-item active">
+                <div class="transactions wallet__transactions" data-autoload="true">
+                    <div class="transactions__list" v-for="item in allList">
+                        <div class="transaction-mini transactions__item"
+                             :class="{'transaction-mini_minus': item.direction == 'down', 'transaction-mini_plus': item.direction == 'up'}">
+                            <div class="transaction-mini__grid transaction-mini__grid_1">
+                                <div class="transaction-mini__grid-item transaction-mini__grid-item_1">
+                                    <div class="transaction-mini__title" v-html="item.name" v-if="item.url.length < 1">
+                                    </div>
+                                    <a :href="item.url" class="transaction-mini__title" v-html="item.name"
+                                       v-if="item.url.length > 1">
+                                    </a>
+                                    <div class="transaction-mini__number">
+                                        <div class="transaction-mini__number-icon">
+                                            <svg class="icon transaction-mini__number-icon-canvas">
+                                                <use xlink:href="/assets/images/icons/icons.svg#icon-arrow-2"></use>
+                                            </svg>
                                         </div>
+                                        <div class="transaction-mini__number-value">
+                                            +50 баллов
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="transaction-mini__grid-item transaction-mini__grid-item_2">
+                                    <div class="transaction-mini__date" v-html="item.date">
                                     </div>
                                 </div>
                             </div>
@@ -85,9 +86,7 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
         </div>
     </div>
 </div>
-
 <script>
-
     const appUserReg = BX.Vue3.BitrixVue.createApp({
         data() {
             return {
@@ -96,57 +95,27 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
                 tabs: {},
 
                 allList: {},
-                roubleList:{},
-                pointsList:{},
+                roubleList: {},
+                pointsList: {},
                 tabActive: 'all',
                 count: 20,
                 allPage: 1,
-                roublePage:{},
-                pointsList:{},
+                roublePage: {},
+                pointsList: {},
             }
         },
-        computed: {
-
-        },
+        computed: {},
         watch: {},
         mounted() {
             this.init()
         },
         methods: {
-            init(){
-                if(!this.el.length){
-                    return ;
-                }
-
-                const _this = this
-
-                this.el.querySelectorAll('.wallet__tabs').each(function() {
-                    let tabs = {
-                        $el: this,
-                    };
-
-                    _this.tabs.nav = {
-                        $items: this.el.querySelectorAll('.wallet__tabs-nav-item'),
-                    };
-
-                    _this.tabs.content = {
-                        $items: this.el.querySelectorAll('.wallet__tabs-content-item'),
-                    };
-
-                    _this.tabs.nav.$items.on('click', function(event) {
-                        let item = {
-                            $el: this,
-                        };
-
-                        item.index = item.$el.index();
-                        item.$el.addClass('active').siblings().removeClass('active');
-                        _this.content.$items.eq(item.index).addClass('active').siblings().removeClass('active');
-                    });
-                });
+            init() {
+                this.getInfo()
             },
-            getInfo(){
+            getInfo() {
                 const _this = this
-                this.send('getInfo', {page: 1, count: 1})
+                this.send('getInfo', {page: 1, count: _this.count})
                     .then(function (response) {
                         _this.errors = response.data.errors ?? response.errors ?? {}
 
